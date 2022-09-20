23e33c4bf12739b525a87547fe679f4271267320
Guilherme Oliveira <guilhermefrancooliveira1994@gmail.com>
2022-09-20 16:50:00 -0300

    .

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,35 @@
-import express from 'express';
+    import express from 'express';
 
-const app = express();
+    const app = express();
+    // HTTP METHODS - GET, POST, PUT, DELETE 
+    // API RESTful // HTTP CODES - 200, 201, 400, 404, 500
+    app.get('/games', (req, res) => {
+        return res.json([]);
+    });
 
-app.get('/ads', (req, res) => {
-return res.json([
-{ id: 1, name: 'Ad 1' },
-{ id: 2, name: 'Ad 2' },
-{ id: 3, name: 'Ad 3' },
-{ id: 4, name: 'Ad 4' },
-{ id: 5, name: 'Ad 5' },
-])
-});
+    app.post('/ads', (req, res) => {
+        return res.status(400).json([]);
+    });
 
-app.listen(3333);
+    app.get('/games/:id/ads', (req, res) => {
+
+        //const gameId = req.params.id;
+        
+
+    return res.json([
+    { id: 1, name: 'Ad 1' },
+    { id: 2, name: 'Ad 2' },
+    { id: 3, name: 'Ad 3' },
+    { id: 4, name: 'Ad 4' },
+    { id: 5, name: 'Ad 5' },
+    ])
+    });
+
+    app.get('/ads/:id/discord', (req, res) => {
+    //const gameId = req.params.id;
+
+    return res.json([
+    ])
+    });
+
+    app.listen(3333);
