cea9cc9486dfb2d7202cef932cda677fbd6d755f
Guilherme Oliveira <guilhermefrancooliveira1994@gmail.com>
2022-09-21 11:28:03 -0300

    .

---
+++ server/src/utils/convert-minute-to-hour-string.ts
@@ -0,0 +1,5 @@
+export function convertMinutesStringToHours(minutesAmount: number) {
+  const hours = Math.floor(minutesAmount / 60);
+  const minutesLeft = minutesAmount % 60;
+  return `${String(hours).padStart(2, '0')}:${String(minutesLeft).padStart(2, '0')}`;
+}
